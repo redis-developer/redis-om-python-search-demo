39b9561ad89be67cc11b63d7909a7aab0b9cc31d
Simon Prickett <simon@redislabs.com>
2021-12-09 23:28:14 +0000

    Tidied up loader.

--- load_adoptables.py
+++ load_adoptables.py
@@ -7,17 +7,7 @@ with open('animal_data.csv') as csv_file:
     animal_reader = csv.DictReader(csv_file)
 
     for animal in animal_reader:
-        adoptable = Adoptable(
-            name = animal["name"],
-            species = animal["species"],
-            age = animal["age"],
-            weight = animal["weight"],
-            sex =  animal["sex"],
-            fee =  animal["fee"],
-            children =  animal["children"],
-            other_animals =  animal["other_animals"],
-            description = animal["description"]
-        )
+        adoptable = Adoptable(**animal)
         
         print(f"{animal['name']} -> {adoptable.pk}")
         adoptable.save()

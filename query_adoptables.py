1969e1501f7f7941aca4523516d2fb7de25f0275
Simon Prickett <simon@redislabs.com>
2021-12-09 23:28:53 +0000

    Got full text search query working.

--- query_adoptables.py
+++ query_adoptables.py
@@ -2,28 +2,12 @@ from adoptable import Adoptable
 
 def show_results(results):
     for adoptable in results:
-        print(f"{adoptable.name}, {adoptable.species}, {adoptable.age} years old: {adoptable.description}")
+        print(adoptable)
         print("")
 
 def find_by_name():
     print("find_by_name:")
     return Adoptable.find(Adoptable.name == "Luna").all()
-
-def find_dogs_in_age_range():
-    print("find_dogs_in_age_range:")
-    return Adoptable.find(
-        (Adoptable.species == "dog") & 
-        (Adoptable.age > 8) & 
-        (Adoptable.age < 11)
-    )
-
-def find_cats_good_with_children():
-    print("find_cats_good_with_children:")
-    # TODO how to get "@description:(-anxious -nervous)"
-    return Adoptable.find(
-        (Adoptable.species == "cat") &
-        (Adoptable.children == "y")
-    )
 
 def find_male_dogs():
     print("find_male_dogs:")
@@ -32,7 +16,26 @@ def find_male_dogs():
         (Adoptable.sex == "m")
     )
 
+def find_dogs_in_age_range():
+    print("find_dogs_in_age_range:")
+    return Adoptable.find(
+        (Adoptable.species == "dog") & 
+        (Adoptable.age > 8) & 
+        (Adoptable.age < 11)
+    ).sort_by("age")
+
+def find_cats_good_with_children():
+    print("find_cats_good_with_children:")
+    return Adoptable.find(
+        (Adoptable.species == "cat") &
+        (Adoptable.children == "y") & 
+        (Adoptable.description % "play") &
+        ~(Adoptable.description % "anxious") &
+        ~(Adoptable.description % "nervous")
+    )
+
+
 #show_results(find_by_name())
+#show_results(find_male_dogs())
 #show_results(find_dogs_in_age_range())
-#show_results(find_cats_good_with_children())
-show_results(find_male_dogs())
+show_results(find_cats_good_with_children())

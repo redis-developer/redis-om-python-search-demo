b874d81a09669881de602e2f542a9ff4209eaf37
Simon Prickett <simon@redislabs.com>
2022-09-17 22:21:15 +0100

    Modified key naming strategy.

--- adoptable.py
+++ adoptable.py
@@ -10,3 +10,9 @@ class Adoptable(HashModel):
     children: str = Field(index=True)
     other_animals: str = Field(index=True)
     description: str = Field(index=True, full_text_search=True)
+
+    # Extra configuration to specify how to generate key
+    # names when saving an instance of the model in Redis.
+    class Meta:
+        global_key_prefix="demo"
+        model_key_prefix="adoptable"
